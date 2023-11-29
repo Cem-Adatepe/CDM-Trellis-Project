21d07261bf88308f6468948465c85d90b1026299
jchanke <josephc2@andrew.cmu.edu>
2023-11-29 14:08:57 -0500

    Implemented drop_balls, now accepts sequence of actions

--- trellis.py
+++ trellis.py
@@ -11,8 +11,8 @@ import argparse
 class Trellis:
     """
     Defines a Trellis class.
-     - Following the handout, a Trellis is a grid of 'X's, so the default trellis
-       (the 1x2 trellis) has 3 'rows' and 3 'columns'.
+     - Following the handout, a Trellis is a grid of 'X's, so the default 
+       trellis (the 1x2 trellis) has 3 'rows' and 3 'columns'.
      - STATE : left == False, right == True
     """
     def __init__(self, h=1, w=2):
@@ -46,7 +46,7 @@ class Trellis:
         """
         Implements the 'basic action': updates the state and ball position.
         Returns True if there's still a ball in the trellis (i.e. we should
-          continue updating.)
+          continue updating).
         """
         if self.ball_position == None:
             return False
@@ -89,21 +89,59 @@ class Trellis:
         return True
     
     def drop_ball(self, slot):
-        """Iterates the basic action 'update'."""
+        """
+        Iterates the basic action 'update'.
+        Slot can either be
+          an integer, 0 <= slot < self.cols; or
+          a letter, such that 0 <= charToSlot(slot) < self.cols.
+        """
         if self.ball_position != None:
-            print("There's already a ball")
-            return
+            raise AssertionError("drop_ball: There's already a ball")
+        
+        if isinstance(slot, (str)):
+            slot = charToSlot(slot)
+
         if slot not in range(self.cols):
-            print("Invalid slot")
-            return
+            raise ValueError(f"drop_ball: slot '{slotToChar(slot)}' not between 'a' and '{slotToChar(self.rows - 1)}'")
         
         # Add the ball, and iterate update
         self.ball_position = (0,slot)
+        print(self)
+        print('---' * self.cols)
         while self.update():
             print(self)
             print('---' * self.cols)
 
+    def drop_balls(self, slots):
+        """Iterates the 'drop_ball' action."""
+        if slots != "" and not slots.isalpha():
+            raise ValueError(f'drop_balls: slots must be a string of letters')
+        for slot in slots:
+            self.drop_ball(slot)
+
+"""
+HELPER FUNCTIONS 
+ - charToSlot : Converts 'a' to 0, 'b' to 1, etc.
+"""
+def charToSlot(char):
+    """
+    char is a single (upper- or lower-case) char.
+    It returns its corresponding slot: mapping
+      'A' == 'a' == 0
+      'B' == 'b' == 1, etc.
+    """
+    if len(char) != 1 or not char.isalpha():
+        raise ValueError(f'charToSlot: expected an single letter, got: {char}')
+    if char.isupper():
+        return ord(char) - ord('A')
+    return ord(char) - ord('a')
+
+def slotToChar(int):
+    if int not in range(ord('z') - ord('a') + 1):
+        raise ValueError(f'slotToChar: int {int} not between 0 and 25')
+    return chr(int + ord('a'))
+
 if __name__ == "__main__":
-    """Run this only if 'trellis.py' is run directly, not as an import"""
+    """Run this only if 'trellis.py' is run directly, not as an import."""
     trellis = Trellis()
     print(trellis)
