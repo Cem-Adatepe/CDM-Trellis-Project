5f09c172ab491b6977220dcb0a57cd883e7cab33
jchanke <josephc2@andrew.cmu.edu>
2023-11-29 12:06:12 -0500

    Added support for virtual environments

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # CDM-Trellis-Project
 Project for CDM (15-354). Developed by Cem Adatepe and Joseph Chan. 
+
+## Installation
+### Virtual environments
+We use a virtual environment for portability. This means that any installed modules affect this project only! To create a new virtual environment called `.venv`, run
+```
+python3 -m venv .venv
+```
+This creates `.venv` as a new folder in the project directory.
+
+Activate the environment:
+```
+source .venv/bin/activate
+```
+Deactivate the environment:
+```
+deactivate
+```
+### Dependencies
+All required Python modules are listed in `requirements.txt`. To install them, from inside the environment, run
+```
+pip3 install -r requirements.txt
+```
--- trellis.py
+++ trellis.py
@@ -1,51 +1,84 @@
+"""
+@brief  Python module for creating and manipulating Trellis objects, with
+        options for printing the state to the terminal.
+@file   trellis.py
+@author Cem Adatepe, Joseph Chan
+"""
+
+from blessed import Terminal
+import argparse
+
 class Trellis:
-    def __init__(self, h = 1, w = 2):
-        self.height = h
-        self.width = w
-        self.rows = 2 * self.height + 1
+    """
+    Defines a Trellis class.
+     - Following the handout, a Trellis is a grid of 'X's, so the default trellis
+       (the 1x2 trellis) has 3 'rows' and 3 'columns'.
+     - STATE : left == False, right == True
+    """
+    def __init__(self, h=1, w=2):
+        self.height, self.width = h, w
+        self.rows = 2*self.height + 1
         self.cols = self.width + 1
-        self.trellis = [[False for j in range (self.cols - i % 2) ] for i in range(self.rows)]
+        self.trellis = [[False for j in range(self.cols - i % 2)] for i in range(self.rows)]
         self.ball_position = None
-    def print(self):        
+
+    def __str__(self):
+        str = ""
         for i in range(len(self.trellis)):
-            if i % 2 == 1:
-                print("  ", end= "")
+            if i % 2 == 1: str += "  " 
             for j in range(len(self.trellis[i])):
-                print("x" if self.getState(i,j) else "o", end="   ")
-            print("")
-    def flipState(self, i, j):
-        self.trellis[i][j] = not self.trellis[i][j]
-    def getState(self, i, j):
-        return self.trellis[i][j]
+                str += 'o' if self.trellis[i][j] else 'x'
+                str += "   "
+            str += '\n'
+        str = str[:-1]
+        return str
+
     def isStateRight(self):
+        """Returns true iff ball goes right"""
         row, col = self.ball_position
         return self.trellis[row][col]
+
+    def flipState(self, i, j):
+        """Flips the state at (i,j)"""
+        self.trellis[i][j] = not self.trellis[i][j]
+    
     def update(self):
+        """
+        Implements the 'basic action': updates the state and ball position.
+        Returns True if there's still a ball in the trellis (i.e. we should
+          continue updating.)
+        """
         if self.ball_position == None:
             return False
         
-        row,col = self.ball_position
+        row, col = self.ball_position
 
+        # Last (even) layer : remove ball
         if row == self.rows -1:
-            self.flipState(row,col)
             self.ball_position = None
-            return False
-        if row % 2 == 1: #odd layer
+
+        # Odd layer
+        elif row % 2 == 1:
             if self.isStateRight():
                 self.ball_position = (row+1, col+1)
             else:
                 self.ball_position = (row+1, col)
-        else: #even layer
+
+        # Even layer
+        else:
+            # Left-most slot
             if col == 0:
                 if self.isStateRight():
                     self.ball_position = (row+1, col)
                 else:
                     self.ball_position = (row+2, col)
+            # Right-most slot
             elif col == self.cols - 1:
                 if self.isStateRight():
                     self.ball_position = (row+2, col)
                 else:
                     self.ball_position = (row+1, col-1)
+            # Non-edge slot
             else:
                 if self.isStateRight():
                     self.ball_position = (row+1, col)
@@ -56,20 +89,21 @@ class Trellis:
         return True
     
     def drop_ball(self, slot):
+        """Iterates the basic action 'update'."""
         if self.ball_position != None:
             print("There's already a ball")
             return
         if slot not in range(self.cols):
             print("Invalid slot")
             return
+        
+        # Add the ball, and iterate update
         self.ball_position = (0,slot)
         while self.update():
-            self.print()
-            print("-------------------------------------------")
-        return
-        
-            
+            print(self)
+            print('---' * self.cols)
 
-
-trellis = Trellis()
-trellis.print()
+if __name__ == "__main__":
+    """Run this only if 'trellis.py' is run directly, not as an import"""
+    trellis = Trellis()
+    print(trellis)
